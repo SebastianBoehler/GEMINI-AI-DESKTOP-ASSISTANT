5c105e7422a21b4c6b064605b90441e1a09e9517
Sebastian Boehler <basti.boehler@hotmail.de>
2024-05-18 22:28:04 +0200

    Initial commit

---
+++ gemini.py
@@ -0,0 +1,46 @@
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel
+from vertexai.preview.generative_models import Part, FinishReason
+
+def generate():
+    vertexai.init(project="ambient-hulling-423419-q7", location="us-central1")
+    model = GenerativeModel("gemini-1.5-flash-preview-0514")
+
+    user_prompt = "Please provide a detailed summary of the current state of artificial intelligence."
+
+    contents = [
+        {
+            "role": "user",
+            "parts": [
+                {
+                    "text": user_prompt,
+                }
+            ],
+        }
+    ]
+
+    responses = model.generate_content(
+        contents,
+        generation_config=generation_config,
+        # safety_settings=safety_settings,
+        stream=True,
+    )
+
+    for response in responses:
+        print(response.text, end="")
+
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+}
+
+safety_settings = {
+    "HARM_CATEGORY_HATE_SPEECH": "BLOCK_MEDIUM_AND_ABOVE",
+    "HARM_CATEGORY_DANGEROUS_CONTENT": "BLOCK_MEDIUM_AND_ABOVE",
+    "HARM_CATEGORY_HARASSMENT": "BLOCK_MEDIUM_AND_ABOVE",
+    "HARM_CATEGORY_SEXUALLY_EXPLICIT": "BLOCK_MEDIUM_AND_ABOVE",
+}
+
+generate()
---
+++ gemini.ts
@@ -0,0 +1,178 @@
+import {
+  VertexAI,
+  HarmCategory,
+  HarmBlockThreshold,
+  GenerateContentRequest,
+  FunctionDeclaration,
+  FunctionDeclarationSchemaType,
+  RetrievalTool,
+  Tool,
+  Content,
+} from "@google-cloud/vertexai";
+
+// Initialize Vertex with your Cloud project and location
+const vertex_ai = new VertexAI({
+  project: "ambient-hulling-423419-q7",
+  location: "us-central1",
+});
+
+const model = "gemini-1.5-pro-preview-0514";
+
+const generativeModel = vertex_ai.getGenerativeModel({
+  model: model,
+  generationConfig: {
+    maxOutputTokens: 8192,
+    temperature: 1,
+    topP: 0.95,
+  },
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ],
+});
+
+const getWeather: FunctionDeclaration = {
+  name: "getWeather",
+  description: "Get the weather in a specific location",
+  parameters: {
+    type: FunctionDeclarationSchemaType.OBJECT,
+    properties: {
+      location: {
+        type: FunctionDeclarationSchemaType.STRING,
+        description: "The location to get the weather for",
+        nullable: false,
+      },
+    },
+    required: ["location"],
+  },
+};
+
+const fetchTool: FunctionDeclaration = {
+  name: "fetch",
+  description:
+    "Fetch data from a given URL. To load any website, use this tool. please load direct files like *.html. load from public sites. only in a row. Returns the fetched website as text",
+  parameters: {
+    type: FunctionDeclarationSchemaType.OBJECT,
+    properties: {
+      url: {
+        type: FunctionDeclarationSchemaType.STRING,
+        description: "The URL to fetch data from",
+        nullable: false,
+      },
+    },
+    required: ["url"],
+  },
+};
+
+const googleRetrival: RetrievalTool = {
+  retrieval: {
+    vertexAiSearch: {
+      datastore: "",
+    },
+  },
+};
+
+const input: string = "`what does hb-capital.app do? find out for`";
+
+const contents = [
+  {
+    role: "user",
+    parts: [
+      {
+        text: input,
+      },
+    ],
+  },
+];
+
+async function generateContent(tools: Tool[], contents: Content[]) {
+  const req: GenerateContentRequest = {
+    contents,
+    tools,
+  };
+
+  const streamingResp = await generativeModel.generateContentStream(req);
+
+  for await (const item of streamingResp.stream) {
+    // console.log(
+    //   "stream chunk: " + JSON.stringify(item.candidates?.[0].content) + "\n"
+    // );
+  }
+
+  const aggregatedResponse = await streamingResp.response;
+  const functionCalls = aggregatedResponse.candidates?.[0].content.parts.filter(
+    (parts) => parts.functionCall
+  );
+  console.log(JSON.stringify(aggregatedResponse.candidates?.[0].content.parts));
+  console.log(aggregatedResponse.candidates?.[0].content.parts.length);
+
+  if (functionCalls) {
+    for (const call of functionCalls) {
+      if (call.functionCall?.name === "fetch") {
+        // @ts-ignore
+        const url = call.functionCall.args.url;
+        const response = await fetch(url);
+        const text = await response.text();
+        contents.push({
+          role: "function",
+          parts: [
+            {
+              functionCall: {
+                name: "fetch",
+                args: {
+                  url: url,
+                },
+              },
+            },
+          ],
+        });
+        contents.push({
+          role: "function",
+          parts: [
+            {
+              functionResponse: {
+                name: "fetch",
+                response: {
+                  text: text,
+                },
+              },
+            },
+          ],
+        });
+        generateContent([], contents);
+      }
+      if (call.functionCall?.name === "getWeather") {
+        // @ts-ignore
+        const location = call.functionCall.args.location;
+        console.log(location);
+        generateContent([], contents);
+      }
+    }
+  }
+}
+
+generateContent(
+  [
+    {
+      functionDeclarations: [
+        // getWeather,
+        fetchTool,
+      ],
+    },
+  ],
+  contents
+);
